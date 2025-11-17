eddf6feae85082278005e072d21f810d7ce71126
Nikita Petrunyak <ox1om2002@gmail.com>
2025-11-17 08:59:19 +0300

    вынесение логики и настройка Core для nuget

---
+++ Shared/Shared.Core/Validation/ValidationErrorsExtensions.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation.Results;
+using Shared.SharedKernel.Errors;
+
+namespace Shared.Core.Validation;
+
+public static class ValidationErrorsExtensions
+{
+    public static ErrorList GetErrors(this ValidationResult validationResult)
+    {
+        var validationErrors = validationResult.Errors;
+
+        var errors = validationErrors.Select(e =>
+            Error.Validation(e.ErrorCode, e.ErrorMessage, e.PropertyName));
+        
+        return new ErrorList(errors);
+    }
+}
